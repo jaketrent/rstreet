c674e791b99fca12d9f55adf627e3ff79e98b5b7
jaketrent <trent.jake@gmail.com>
2015-02-03 20:40:09 -0700

    load env and validate options

--- lib/rstreet.rb
+++ lib/rstreet.rb
@@ -1,17 +1,40 @@
 require "rstreet/version"
+require "dotenv"
 
 module Rstreet
-  class Debug
+  class Uploader
 
     def initialize(options)
       @options = options
+      load_env if should_load_env?
+      validate_options
     end
 
-    def hello
-      @options.to_h.each do |k, v|
-        puts "#{k} => #{v}"
-      end
-      puts "Rstreet time!"
+    def run
     end
+
+    private
+
+    def load_env
+      Dotenv.load
+      @options.s3_bucket ||= ENV["S3_BUCKET"]
+      @options.aws_key ||= ENV["AWS_ACCESS_KEY_ID"]
+      @options.aws_secret ||= ENV["AWS_SECRET_ACCESS_KEY"]
+    end
+
+    def should_load_env?
+      @options.load_env
+    end
+
+    def validate_options
+      raise ArgumentError, "Specify a source directory in options.src" if @options.src.nil?
+
+      raise ArgumentError, "Specify an S3 Bucket in an env var called S3_BUCKET or options.s3_bucket" if @options.s3_bucket.nil?
+
+      raise ArgumentError, "Specify an AWS Access Key Id in an env var called AWS_ACCESS_KEY_ID or options.aws_key" if @options.aws_key.nil?
+
+      raise ArgumentError, "Specify an AWS Secret Access Key in env var called AWS_SECRET_ACCESS_KEY or options.aws_secret" if @options.aws_secret.nil?
+    end
+
   end
 end
